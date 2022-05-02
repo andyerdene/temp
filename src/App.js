275343357323d25c43ed21ea7da800c1c1f86160
andyerdene <anidas.com@gmail.com>
2022-05-02 12:51:16 +0800

    Initial App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Person from "./Person";
+
+function App() {
+  return (
+    <div className="App">
+      <Person />
+      {/* {message} */}
+    </div>
+  );
+}
+
+export default App;
